1d29f3d139d3b7174b89189dec0e54a9c76d7c0d
grzegorznieuzylanpg <34072044+grzegorznieuzylanpg@users.noreply.github.com>
2018-01-09 11:15:41 +0100

    Update 7.c

--- 7.c
+++ 7.c
@@ -2,7 +2,7 @@
 <<<<<<< HEAD// Data   :27.03.2008 --- zmiana 2121212121
 =======// Data   : 27.03.2008 --- zmiana 4
 >>>>>>> 2e87646aeed1a466ad6ce2780aa7bd26cd1555a
-// (C)2012 mgr Jerzy Wa³aszek
+// (C)2012 mgr Jerzy WaÂ³aszek
 //----------------------------
 
 #include <iostream>
@@ -12,7 +12,7 @@ using namespace std;
 
 void Fermat(unsigned int p)
 {
-  unsigned long long x,y,z,m,n;
+  unsigned long long x,y,z,m,n,o;
 
   x = (unsigned long long)ceil(sqrt(p));
   do
